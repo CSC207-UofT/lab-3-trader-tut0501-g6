117a7261c8796802b25c890f7be6918aa12ba24e
Patrick <patrick.zhou@mail.utoronto.ca>
2021-09-27 19:34:50 -0400

    fill in the methods

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,27 +1,37 @@
 public class Vehicle implements Drivable, Domesticatable, Tradable {
+    private int maxSpeed;
+    public Vehicle() {
+        this.maxSpeed = 10;
+    }
 
     @Override
     public String sound() {
-        return null;
+        return "Vroom";
     }
 
     @Override
     public void upgradeSpeed() {
-
+        this.maxSpeed++;
     }
 
     @Override
     public void downgradeSpeed() {
-
+        this.maxSpeed--;
     }
 
     @Override
     public int getMaxSpeed() {
-        return 0;
+        return this.maxSpeed;
     }
 
     @Override
     public int getPrice() {
-        return 0;
+        return 10;
+    }
+
+    @Override
+    public String toString(){
+        String id = super.toString();
+        return id + " (Max speed: " + this.maxSpeed +")";
     }
 }
