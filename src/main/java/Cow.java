f1a18bf0176e5ec74baaba03bb753ab40ae09022
yde773786 <yde773786@gmail.com>
2021-09-27 19:15:07 -0400

    cow

---
+++ src/main/java/Cow.java
@@ -0,0 +1,12 @@
+public class Cow implements Tradable, Domesticatable{
+
+    @Override
+    public String sound() {
+        return null;
+    }
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+}
