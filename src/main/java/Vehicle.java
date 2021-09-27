afa2c3a104b1c8a3dd24bbbe30afbe0209e9df36
Patrick <patrick.zhou@mail.utoronto.ca>
2021-09-27 19:23:59 -0400

    create a vehicle class

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,27 @@
+public class Vehicle implements Drivable, Domesticatable, Tradable {
+
+    @Override
+    public String sound() {
+        return null;
+    }
+
+    @Override
+    public void upgradeSpeed() {
+
+    }
+
+    @Override
+    public void downgradeSpeed() {
+
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return 0;
+    }
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+}
