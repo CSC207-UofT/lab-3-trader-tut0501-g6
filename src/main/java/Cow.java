d338887f4ea7a1cb965257414856725a25700361
yde773786 <yde773786@gmail.com>
2021-09-27 19:25:42 -0400

    cow booleans

--- src/main/java/Cow.java
+++ src/main/java/Cow.java
@@ -1,12 +1,32 @@
 public class Cow implements Tradable, Domesticatable{
 
+    private boolean isMilkable;
+    public boolean toKill;
+
+    public boolean isToKill() {
+        return toKill;
+    }
+
+    public void setToKill(boolean toKill) {
+        this.toKill = toKill;
+    }
+
     @Override
     public String sound() {
-        return null;
+        return "MOOOO";
+    }
+
+    public boolean isMilkable() {
+        return isMilkable;
+    }
+
+    public void setMilkable(boolean milkable) {
+        isMilkable = milkable;
     }
 
     @Override
     public int getPrice() {
-        return 0;
+        return 50;
     }
+
 }
